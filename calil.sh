3464c9637ea1280d1ff104a0e3d565cc3c940ab4
xu9010 <xu9010@gmail.com>
2025-07-11 19:19:07 +0800

    Update calil.sh

--- calil.sh
+++ calil.sh
@@ -28,7 +28,7 @@ env/bin/genpac \
 	--user-rule-from user-rule.txt \
 	-o gfwlist_1081.pac
 # sed -e '3d' -i gfwlist_1081.pac
-sed -e '5d' -e '3d' -i gfwlist_1081.pac # 删除带无用日期的注释
+# sed -e '5d' -e '3d' -i gfwlist_1081.pac # 删除带无用日期的注释
 
 env/bin/genpac \
 	--format pac \
